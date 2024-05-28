09ad3bf5a3a146e202b5dd02ef1a958299dcaf20
martisu <marti.surralles@autonoma.cat>
2024-05-28 16:09:00 +0200

    ara be

--- laplace_MPI.c
+++ laplace_MPI.c
@@ -74,7 +74,7 @@ int main(int argc, char** argv)
     // INITIALIZE MPI
     int size, rank, task;		
     MPI_Status s;
-        MPI_Request request;
+    MPI_Request request;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);	
