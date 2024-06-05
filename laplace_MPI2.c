26101ff746b3dc98b41bd6679a78ff57d44ffeb3
martisu <marti.surralles@autonoma.cat>
2024-06-06 01:44:33 +0200

    versio v2

---
+++ laplace_MPI2.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
+
+float stencil ( float v1, float v2, float v3, float v4 )
+{
+    return (v1 + v2 + v3 + v4) / 4;
+}
+
+void laplace_step ( float *in, float *out, int n, int m)
+{
+    int i, j;
+    for ( i=1; i < n-1; i++ )
+        for ( j=1; j < m-1; j++ )
+        out[i*m+j]= stencil(in[i*m+j+1], in[i*m+j-1], in[(i-1)*m+j], in[(i+1)*m+j]);
+}
+
+float laplace_error ( float *old, float *new, int n, int m )
+{
+    int i, j;
+    float error=0.0f;
+    for ( i=1; i < n-1; i++ )
+        for ( j=1; j < m-1; j++ )
+        error = fmaxf( error, sqrtf( fabsf( old[i*m+j] - new[i*m+j] )));
+    return error;
+}
+
+void laplace_copy ( float *in, float *out, int n, int m )
+{
+    int i, j;
+    for ( i=1; i < n-1; i++ )
+        for ( j=1; j < m-1; j++ )
+        out[i*m+j]= in[i*m+j];
+}
+
+
+void laplace_init ( float *in, int n, int m )
+{
+    int i, j;
+    const float pi  = 2.0f * asinf(1.0f);
+    memset(in, 0, n*m*sizeof(float));
+    for (j=0; j<m; j++)  in[    j    ] = 0.f;
+    for (j=0; j<m; j++)  in[(n-1)*m+j] = 0.f;
+    for (i=0; i<n; i++)  in[   i*m   ] = sinf(pi*i / (n-1));
+    for (i=0; i<n; i++)  in[ i*m+m-1 ] = sinf(pi*i / (n-1))*expf(-pi);
+}
+
+int main(int argc, char** argv)
+{
+    // Init time
+    double t1 = MPI_Wtime();
+
+    // Init variables
+    int n = 4096, m = 4096;
+    const float pi  = 2.0f * asinf(1.0f);
+    const float tol = 3.0e-3f;
+
+    float error= 1.0f;
+
+    int i, j, iter_max=100, iter=0;
+    float *A, *Anew, *Aext, *Anewext, *row0, *rown, *prev, *post; 
+
+    // get runtime arguments: n, m and iter_max
+    if (argc>1) {  n        = atoi(argv[1]); }
+    if (argc>2) {  m        = atoi(argv[2]); }
+    if (argc>3) {  iter_max = atoi(argv[3]); }
+
+    // INITIALIZE MPI (size = nproc)
+    int nproc, rank, task;		
+    MPI_Status s;    
+    MPI_Request request;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Number of rows per process
+    int split = n / nproc;
+
+    // Declare arrays
+    A    = (float*) malloc(n * m * sizeof(float) );
+    Anew = (float*) malloc(n * split * sizeof(float) );
+    Aext = (float*) malloc(n * (split+2) * sizof(float));
+    Anewext = (float*) malloc(n * (split+2) * sizeof(float));
+    row0 = (float*) malloc(m * sizeof(float));
+    rown = (float*) malloc(m * sizeof(float));
+    prev = (float*) malloc(m * sizeof(float));
+    post = (float*) malloc(m * sizeof(float));
+    
+    // set boundary conditions
+    if (rank == 0) {
+        laplace_init (A, n, m);
+        printf("Jacobi relaxation Calculation: %d rows x %d columns mesh,"
+            " maximum of %d iterations\n",
+            n, m, iter_max );
+    }
+
+        // Main loop: iterate until error <= tol a maximum of iter_max iterations
+    while ( error > tol && iter < iter_max ) {
+        // Scatter A
+        MPI_Scatter(A, split*m, MPI_FLOAT, A, split*m, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+        // Send and recieve prev and post
+        memcpy(row0, A, m*sizeof(float));
+        memcpy(rown, A+(m*(split-1)), m*sizeof(float));
+        if (rank > 0) {
+            MPI_Send(row0, m, MPI_FLOAT, rank-1, 0, MPI_COMMM_WORLD);
+            MPI_Irecv(post, m, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, &request);
+            }
+        if (rank < (nproc-1)) {
+            MPI_Send(rown, m, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD);
+            MPI_Irecv(prev, m, MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, &request);
+        }
+
+        // Extend A with prev and post
+        memcpy(Aext, prev, m*sizeof(float));
+        memcpy(Aext+m, A, split*m*sizeof(float));
+        memcpy(Aext+(split*m), post, m*sizeof(float));
+
+        // Create Anewext as a copy of Aext
+        memcpy(Anewext, A, (split+2)*m*sizeof(float));
+
+        // Compute new values using main matrix and writing into auxiliary matrix (with Aext and Anewext)
+        laplace_step (Aext, Anewext, split+2, m);
+
+        // Compute error = maximum of the square root of the absolute differences (with Aext and Anewext)
+        error = 0.0f;
+        error = laplace_error (Aext, Anewext, split+2, m);
+
+        // Create Anew by removing the extensions from Anewext
+        memcpy(Anew, Anewext+m, split*m*sizeof(float));
+
+        // Copy from auxiliary matrix to main matrix
+        // laplace_copy (Anew, A, n, m);
+        memcpy(A, Anew, split*m*sizeof(float));
+
+        // Gather A from 0
+        MPI_Gather(A, split*m, MPI_FLOAT, A, split*m, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        
+        // if number of iterations is multiple of 10 then print error on the screen
+        iter++;
+        if (iter % (iter_max/10) == 0)
+        printf("%5d, %0.6f\n", iter, error);
+    } // while
+
+    if (rank == 0){
+        double t2 = MPI_Wtime();
+        print("Finished calculation! Time: %fs", t2-t1)
+    }
+
+    free(A);
+    free(Anew);
+}
